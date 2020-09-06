cbb244cc3e5008046b753ff8b0718414605a0d58
fsadannn <fsadannn@gmail.com>
2020-09-06 17:14:03 -0400

    change to table to show all info

--- popup.js
+++ popup.js
@@ -1,6 +1,10 @@
+function round(number, digits=2){
+    return Math.round((number+Number.EPSILON)*10**digits)/10**digits;
+}
+
 document.addEventListener('DOMContentLoaded', function()
 {
-    var ctx = document.getElementById("chart").getContext('2d');
+    /*var ctx = document.getElementById("chart").getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'doughnut',
         data: {
@@ -17,10 +21,30 @@ document.addEventListener('DOMContentLoaded', function()
         }
     });
 
-    document.getElementById('test').textContent = 'SUCCEED';
+    document.getElementById('test').textContent = 'SUCCEED';*/
 
 
     fetch('http://127.0.0.1:5000')
-  .then(response => response.json())
-  .then(data => {console.log(data.internet.data.values); document.getElementById('test').textContent = data.internet.data.values.normal.cant;});
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.internet.data.values);
+        document.getElementById('lteo').textContent = data.internet.data.values.only_lte.cant+" MB";
+        document.getElementById('lteod').textContent = round(data.internet.data.values.only_lte.delta);
+        document.getElementById('an').textContent = data.internet.data.values.all_networks.cant+" MB";
+        document.getElementById('and').textContent = round(data.internet.data.values.all_networks.delta);
+        document.getElementById('lteb').textContent = data.internet.data.values.lte.cant+" MB";
+        document.getElementById('ltebd').textContent = round(data.internet.data.values.lte.delta);
+        document.getElementById('nb').textContent = data.internet.data.values.national_data.cant+" MB";
+        document.getElementById('nbd').textContent = round(data.internet.data.values.national_data.delta);
+
+        document.getElementById('cerdit').textContent = data.internet.credit.values.credit_normal.cant;
+        document.getElementById('cerdit').textContent = round(data.internet.credit.values.credit_normal.delta);
+        document.getElementById('cerditb').textContent = data.internet.credit.values.credit_bonus.cant;
+        document.getElementById('cerditbd').textContent = round(data.internet.credit.values.credit_bonus.delta);
+        document.getElementById('min').textContent = data.internet.others.values.minutes.cant;
+        document.getElementById('mind').textContent = round(data.internet.others.values.minutes.delta);
+        document.getElementById('sms').textContent = data.internet.others.values.sms.cant;
+        document.getElementById('smsd').textContent = round(data.internet.others.values.sms.delta);
+        //document.getElementById('test').textContent = data.internet.data.values.normal.cant+'  delta='+round(data.internet.data.values.normal.delta);
+    });
 });
