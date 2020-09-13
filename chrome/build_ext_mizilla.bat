a3d4dd5cf5fca9b755925366034de2a62427d74e
fsadannn <fsadannn@gmail.com>
2020-09-13 17:50:18 -0400

    restructure for browsers

---
+++ chrome/build_ext_mizilla.bat
@@ -0,0 +1,2 @@
+rem web-ext sign --api-key user:16292671:49 --api-secret 9816313905068abdd704c1aaa3938f4c9438143ee0d81f53335abf912c145323 --ignore-files TODO --ignore-files build_ext_mizilla.bat --ignore-files .gitignore
+web-ext build --ignore-files=build_ext_mizilla.bat .gitignore TODO --overwrite-dest
