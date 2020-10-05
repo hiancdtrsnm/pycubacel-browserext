cabcd8165715eb78652522cb53a10c1ba62be6df
Frank S <fsadannn@gmail.com>
2020-10-05 18:05:08 -0400

    update build_Ext.sh

--- firefox/build_ext.sh
+++ firefox/build_ext.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
-npm install -g web-ext
-web-ext sign --ignore-files=build_ext_mizilla.bat .gitignore TODO --api-key=$API_KEY --api-secret=$API_SECRET
+# npm install -g web-ext
+web-ext sign --ignore-files=build_ext.bat build_ext.sh --api-key=$API_KEY --api-secret=$API_SECRET
